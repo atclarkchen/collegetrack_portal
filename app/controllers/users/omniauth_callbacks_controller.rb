07e78f3d674c29f053a56ec456b9534ea7ddf4ea
forgottn <pduong@berkeley.edu>
2015-03-12 16:00:02 -0700

    Javascript Popup functionality for oauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,14 +3,24 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       # You need to implement the method below in your model (e.g. app/models/user.rb)
       @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
 
-      if @user.persisted?
+      if not @user.nil?
         #flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
         sign_in @user, :event => :authentication
-        redirect_to email_index_path
+        @after_sign_in_url = email_index_path
+        if request.env['omniauth.params']['popup']
+          render 'callback', :layout => false
+        else
+          redirect_to @after_sign_in_url
+        end
       else
         session["devise.google_data"] = request.env["omniauth.auth"]
         flash[:notice] = "This email is not authorized. Please log out and log in with an authorized account. Contact an admin if not yet authorized"
-        redirect_to root_path
+        @after_sign_in_url = root_path
+        if request.env['omniauth.params']['popup']
+          render 'callback', :layout => false
+        else
+          redirect_to @after_sign_in_url
+        end
       end
   end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -232,7 +232,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :google_oauth2, Rails.application.secrets.client_id, Rails.application.secrets.client_secret
+  config.omniauth :google_oauth2, Rails.application.secrets.client_id, Rails.application.secrets.client_secret, :display => 'popup'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
