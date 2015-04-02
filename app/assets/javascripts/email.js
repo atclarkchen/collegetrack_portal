ddd15929355cc54856dde8eb1eaf194ef8b2cbbb
forgottn <pduong@berkeley.edu>
2015-04-01 22:07:49 -0700

    UI tweaks

--- app/assets/javascripts/email.js
+++ app/assets/javascripts/email.js
@@ -1,13 +1,24 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-var FilterToggle = {
+var EmailFilter = {
 	setup: function() {
-		$('#email_form').on('click', '#change_filter', FilterToggle.slideOpen);
+		$('#email_form').on('click', '#change_filter', EmailFilter.slideOpen);
+		$('#filters').on('click', '.x', EmailFilter.remove);
 	},
 	slideOpen: function() {
 	    $('#accordian').animate({width: 'toggle'});
+    },
+    remove: function() {
+    	var filterText = $(this).prev().text();
+    	$(this).parent().remove();
+    	$(".selected").each(function() {
+    		if ($(this).text() == filterText) {
+				$(this).toggleClass('selected');
+				return false;
+    		}
+    	});
     }
 };
 
-$(FilterToggle.setup);
+$(EmailFilter.setup);
