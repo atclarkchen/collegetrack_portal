a6b3af013f4834cd32c4a1fa448bf163405dfd44
Jason Chern <jason.chern93@gmail.com>
2015-04-03 00:44:44 -0700

    rerouted admin tab, added admin controller and view

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -1,20 +1,4 @@
 class EmailController < ApplicationController
   def index
   end
-
-  def admin
-  	@users = User.all
-  end
-
-  def new
-  	@user = User.new(:name => params[:user][:name], :email => params[:user][:email], :role => params[:user][:role], :password => 'password')
-  	@user.save!
-  	redirect_to email_admin_path
-  end
-
-  def destroy
-  	@user = User.find(params[:user])
-  	@user.destroy
-  	redirect_to email_admin_path
-  end
 end
