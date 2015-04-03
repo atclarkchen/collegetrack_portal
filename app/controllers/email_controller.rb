2d8f1b7b7f5643ce1c966f34e9bc93993ff34399
KyleC <hchang239@berkeley.edu>
2015-04-02 20:53:10 -0700

    finish send email

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -8,16 +8,21 @@ class EmailController < ApplicationController
   end
 
   def send_message
-    # Gmail.connect(:xoauth2, current_user.email, current_user.token.fresh_token) do |gmail|
-      # gmail.deliver do
-      #   to "llss14@daum.net"
-      #   bcc "hchang239@berkeley.edu, llss14@naver.com"
-      #   subject "This is test-email from CollegeTrack"
-      #   html_part do
-      #     content_type 'text/html; charset=UTF-8'
-      #     body "<p>Text of <em>html</em> message.</p>"
-      # end
-    # end
+    email = params[:email]
+    Gmail.connect(:xoauth2, current_user.email, current_user.token.fresh_token) do |gmail|
+      gmail.deliver do
+        to  email[:to]
+        cc  email[:cc]
+        bcc email[:bcc]
+
+        subject email[:subject]
+        html_part do
+          content_type 'text/html; charset=UTF-8'
+          body email[:body]
+        end
+      end
+    end
+
     flash[:notice] = "Message sent successfully"
     redirect_to email_index_path
   end
