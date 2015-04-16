710ea1a6632be31825107e8e98eaaa96f1a8478d
Kyle Chang <hchang239@berkeley.edu>
2015-04-15 18:03:55 -0700

    add comments

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -25,6 +25,10 @@ module EmailHelper
   end
 
   def save_draft(email)
+    # Create Mail object with form data
+    # Create Draft model with mail.to_s
+    # Add attachment(s) to the current Draft model
+
     Gmail.connect(:xoauth2, current_user.email, current_user.token.fresh_token) do |gmail|
       draft = gmail.compose do
         to  email[:to]
