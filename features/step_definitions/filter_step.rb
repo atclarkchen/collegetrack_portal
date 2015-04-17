f325d5ffa7a38f6387364d3b1edee2ede38df079
forgottn <pduong@berkeley.edu>
2015-04-16 21:46:07 -0700

    split the string

--- features/step_definitions/filter_step.rb
+++ features/step_definitions/filter_step.rb
@@ -34,6 +34,7 @@ Then /^(?:|I )click the x button on "(.*)"$/ do |filters|
 end
 
 Then /^the recipient fields should contain: (.*)$/ do |emails|
+  emails = emails.split(", ")
   within ('#recipient_header') do
     emails.each do |email|
       expect(page).to have_content(email)
