56c143d792953e54d8450eaf39c0cd33e615f082
forgottn <pduong@berkeley.edu>
2015-04-22 01:51:28 -0700

    Added sweet-alert

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,46 @@
+var Admin = {
+  setup: function() {
+    $('#users').on('click', '.x_button', Admin.destroy);
+    $('#add_user_form').on('submit', Admin.submit);
+  },
+  destroy: function() {
+    var user = $(this).parent().prev().prev().text();
+    swal({
+      title: "Are you sure?",
+      text: "Your will have to re-add the user!",
+      type: "error",
+      showCancelButton: true,
+      confirmButtonClass: "btn-danger",
+      confirmButtonText: "Yes",
+      closeOnConfirm: false
+    },
+    function(){
+      swal("Deleted!", "User has been deleted.", "success");
+      $.ajax({
+        url: "/admin/",
+        method: "delete",
+        data: {"user": user},
+        success: function(html) {
+          $('#users').html(html);
+          $('#add_user_form')[0].reset();
+        }
+      });
+    });
+  },
+  submit: function() {
+    $.ajax({
+      url: "/admin/",
+      method: "post",
+      data: $('#add_user_form').serialize(),
+      success: function(html) {
+        toastr.options.positionClass = "toast-bottom-right";
+        toastr.success('Email added');
+        $('#users').html(html);
+        $('#add_user_form')[0].reset();
+      }
+    });
+    return false;
+  }
+}
+
+$(Admin.setup);
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,4 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require toastr
+//= require sweet-alert.min
