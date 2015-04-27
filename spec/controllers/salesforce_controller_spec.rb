2e4a470b05cff27eecc762f3bf61816ee149bd15
Jason Chern <jason.chern93@gmail.com>
2015-04-26 18:32:46 -0700

    travis fix 5

--- spec/controllers/salesforce_controller_spec.rb
+++ spec/controllers/salesforce_controller_spec.rb
@@ -6,25 +6,53 @@ describe SalesforceController do
     login_admin
   end
 
-  describe 'save_password' do
-    it 'should redirect to login page' do
+  context 'Salesforce password is up to date' do
+
+    describe 'save password' do
+      it 'should redirect to login page' do
         post :save_password, :confirm_password => {:confirm_password => "asdfasdf"}, :password => {:password => "asdfasdf"}, :token => {:token => "token"}
         expect(response).to redirect_to root_path
+      end
+
+      it 'should not modify the Salesforce password or token' do
+        post :save_password, :confirm_password => {:confirm_password => "asdfasdf"}, :password => {:password => "asdfasdf"}, :token => {:token => "token"}
+        expect(ENV['SALESFORCE_PASSWORD']).to_not eq("asdfasdf")
+        expect(ENV['SALESFORCE_SECURITY_TOKEN']).to_not eq("token")
+      end
     end
 
-    it 'should update the password' do
+    describe 'reset salesforce' do
+      it 'should redirect to email page' do
+          get :reset_salesforce
+          expect(response).to render_template(:reset_salesforce)
+      end
+    end
+  end
+
+  context 'Salesforce password is outdated' do
+    
+    before :each do
       ENV['SALESFORCE_PASSWORD'] = 'outdated'
-      post :save_password, :confirm_password => {:confirm_password => "asdfasdf"}, :password => {:password => "asdfasdf"}, :token => {:token => "token"}
-      expect(ENV['SALESFORCE_PASSWORD']).to eq("asdfasdf")
-      expect(ENV['SALESFORCE_SECURITY_TOKEN']).to eq("token")
+    end
+
+    describe 'save_password' do
+      it 'should redirect to login page' do
+          post :save_password, :confirm_password => {:confirm_password => "asdfasdf"}, :password => {:password => "asdfasdf"}, :token => {:token => "token"}
+          expect(response).to redirect_to root_path
+      end
+
+      it 'should update the Salesforce password' do
+        post :save_password, :confirm_password => {:confirm_password => "asdfasdf"}, :password => {:password => "asdfasdf"}, :token => {:token => "token"}
+        expect(ENV['SALESFORCE_PASSWORD']).to eq("asdfasdf")
+        expect(ENV['SALESFORCE_SECURITY_TOKEN']).to eq("token")
+      end
+    end
+
+    describe 'reset salesforce' do
+      it 'should render the reset Salesforce page' do
+          get :reset_salesforce
+          expect(response).to render_template(:reset_salesforce)
+      end
     end
   end
-
-  describe 'reset salesforce' do
-    it 'should redirect to email page' do
-        get :reset_salesforce
-        expect(response).to redirect_to email_index_path
-    end
-  end
-
 end
