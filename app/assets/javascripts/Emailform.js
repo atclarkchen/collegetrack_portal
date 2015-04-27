a8ae01cfe5aa32ba4a54b55ba9229a889b7c7c01
Kyle Chang <hchang239@berkeley.edu>
2015-04-27 01:02:04 -0700

    make whole form with dropzone

--- app/assets/javascripts/Emailform.js
+++ app/assets/javascripts/Emailform.js
@@ -1,33 +1,45 @@
-$(document).ready(function() {
-  // Disable autoDiscover
-  Dropzone.autoDiscover = false;
+Dropzone.options.emailForm = {
+  uploadMultiple: true,
+  paramName: "email[files]",
+  maxFilesize: 4,
+  addRemoveLinks: true,
+  autoProcessQueue: false,
+  parallelUploads: 50,
+  maxFiles: 10,
+  previewsContainer: "#dropzone-preview",
 
-  // grap our upload form by its id
-  $("#email_form").dropzone({
-    uploadMultiple: true,
-    paramName: "email[files]",
-    maxFilesize: 4,
-    addRemoveLinks: true,
-    autoProcessQueue: false,
-    parallelUploads: 50,
-    maxFiles: 10,
-    previewsContainer: "#dropzone-preview",
+  init: function() {
+    var myDropzone = this;
 
-    init: function() {
-      var myDropzone = this;
+    $('input[type="submit"]').addEventListener("click", function(e){
+      myDropzone.processQueue();
+    });
+  }
+};
 
-      $('input[type="submit"]').on("click", function(e) {
-        e.preventDefault();
-        e.stopPropagation();
+// $(document).ready(function() {
+//   // Disable autoDiscover
+//   Dropzone.autoDiscover = false;
 
-        if(myDropzone.getQueuedFiles().length > 0) {
-          window.alert("files in queue");
-          myDropzone.processQueue();
-        }
-        // } else {
-        //   $('#email_form').submit();
-        // }
-      });
-    }
-  });
-});
+//   // grap our upload form by its id
+//   $("#email_form").dropzone({
+//     uploadMultiple: true,
+//     paramName: "email[files]",
+//     maxFilesize: 4,
+//     addRemoveLinks: true,
+//     // autoProcessQueue: false,
+//     parallelUploads: 50,
+//     maxFiles: 10,
+//     previewsContainer: "#dropzone-preview",
+
+//     // init: function() {
+//     //   var myDropzone = this;
+
+//     //   var submitForm = document.querySelector("#send_draft");
+
+//     //   submitForm.addEventListener('click', function(e) {
+
+//     //   });
+//     // }
+//   });
+// });
