07273da6b265783792f8139e6e18e72ad05e440c
Jason Chern <jason.chern93@gmail.com>
2015-04-26 18:37:47 -0700

    travis fix 6

--- spec/controllers/salesforce_controller_spec.rb
+++ spec/controllers/salesforce_controller_spec.rb
@@ -24,7 +24,7 @@ describe SalesforceController do
     describe 'reset salesforce' do
       it 'should redirect to email page' do
           get :reset_salesforce
-          expect(response).to render_template(:reset_salesforce)
+          expect(response).to redirect_to email_index_path
       end
     end
   end
