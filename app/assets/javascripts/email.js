03c41b691fce28c2784717ec8953389a6d82da18
forgottn <pduong@berkeley.edu>
2015-04-29 15:41:27 -0700

    Merged with admin_tab

--- app/assets/javascripts/email.js
+++ app/assets/javascripts/email.js
@@ -80,7 +80,7 @@ var RecipientField = {
             var prev = ($(this).prev().length === 0) ? $(this) : $(this).prev();
             for (var i in emails) {
                 if (filter.test(emails[i])) {
-                    $(prev).after("<div class='edit_box'><span class='ui_fil'><div class ='left_fil'>" + emails[i] + "</div><div class='x'></div></span><input name='message[" + field + "][]' type='hidden' value='" + emails[i] + "'></div>");
+                    $(prev).after("<div class='edit_box'><span class='ui_fil'><div class ='left_fil'>" + emails[i] + "</div><div class='x'></div></span><input name='email[" + field + "][]' type='hidden' value='" + emails[i] + "'></div>");
                 } else {
                     var restEmails = emails.slice(i);
                     restEmails = restEmails.join(" ");
